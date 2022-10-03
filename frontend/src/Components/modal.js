a5980a48bb366cf0bfb7bcbd6342756b41ffdc8f
SoJeong Lee <sojeong.lee017@gmail.com>
2022-10-04 02:02:58 +0900

    function : Modal.js

--- frontend/src/Components/modal.js
+++ frontend/src/Components/modal.js
@@ -14,29 +14,29 @@ const Modal = ({}) => {
   const onPlaceChanged = (placeInstance) => {
     console.log(placeInstance.getPlace());
   };
-  if (isLoaded) {
-    return (
-      <div className={style.container}>
-        <h1>Enter Your Information</h1>
-        <input type="text" placeholder="Your name"></input>
-        <Autocomplete
-          onLoad={onLoad}
-          onPlaceChanged={onPlaceChanged}
-          fields={["geometry", "name", "address_components"]}
-          types={["establishment"]}
-        >
-          <input type="text" placeholder="Your address"></input>
-        </Autocomplete>
-        <button
-          onClick={(place) => {
-            console.log(place.getPlace());
-          }}
-        >
-          Save
-        </button>
-      </div>
-    );
-  }
+  return isLoaded ? (
+    <div className={style.container}>
+      <h1>Enter Your Information</h1>
+      <input type="text" placeholder="Your name"></input>
+      <Autocomplete
+        onLoad={onLoad}
+        onPlaceChanged={onPlaceChanged}
+        fields={["geometry", "name", "address_components"]}
+        types={["establishment"]}
+      >
+        <input type="text" placeholder="Your address"></input>
+      </Autocomplete>
+      <button
+        onClick={(place) => {
+          console.log(place.getPlace());
+        }}
+      >
+        Save
+      </button>
+    </div>
+  ) : (
+    console.log(loadError)
+  );
 };
 
 export { Modal };
